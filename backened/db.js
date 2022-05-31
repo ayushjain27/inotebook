ed5598d64eeff99737f7c6642bb0c19b4f62172e
ayushjain27 <ayushjain201574@gmail.com>
2022-05-31 10:42:32 +0530

    express

---
+++ backened/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/"
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo successfully");
+    })
+}
+
+module.exports = connectToMongo;
