989ce803ff659ddcbd4b93c5a75d67ca21cc5138
ayushjain27 <ayushjain201574@gmail.com>
2022-05-31 12:47:37 +0530

    adding routes

--- backened/index.js
+++ backened/index.js
@@ -1,13 +1,17 @@
 const connectToMongo = require('./db');
-connectToMongo();
-
 const express = require('express')
+
+connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello Ayush!')
+// })
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
