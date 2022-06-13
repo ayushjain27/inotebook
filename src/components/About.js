7afb43dba815c174167f5f4508fa689d1d0d2186
ayushjain27 <ayushjain201574@gmail.com>
2022-06-13 18:26:32 +0530

    Used uselocationHook and designed a home page

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,26 @@
-import React, { useContext, useEffect} from 'react'
-import noteContext from '../context/notes/noteContext'
+// import React, { useContext, useEffect} from 'react'
+// import noteContext from '../context/notes/noteContext'
+import React from 'react'
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
   return (
     <div>
-      This is About {a.state.name} and he is in class {a.state.class}
+      This is About page
     </div>
   )
 }
 
 export default About
+
+// const About = () => {
+//   const a = useContext(noteContext)
+//   useEffect(() => {
+//     a.update();
+//     // eslint-disable-next-line
+//   }, [])
+//   return (
+//     <div>
+//       This is About {a.state.name} and he is in class {a.state.class}
+//     </div>
+//   )
+// }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,24 +3,32 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Ayush",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "AyushJain",
-                "class": "5b"
-            })
-        }, 1000);
-    }
     return (
-    <NoteContext.Provider value={{state, update}}>
+    <NoteContext.Provider value={{}}>
         {props.children}
     </NoteContext.Provider>
     )
 };
 
 export default NoteState;
+
+// const NoteState = (props) => {
+//     const s1 = {
+//         "name": "Ayush",
+//         "class": "5b"
+//     }
+//     const [state, setState] = useState(s1);
+//     const update = () => {
+//         setTimeout(() => {
+//             setState({
+//                 "name": "AyushJain",
+//                 "class": "5b"
+//             })
+//         }, 1000);
+//     }
+//     return (
+//     <NoteContext.Provider value={{state, update}}>
+//         {props.children}
+//     </NoteContext.Provider>
+//     )
+// };
